a7ccda2902362d02857b4f7a37e8eadd2b944e5c
uday <uday.p@simformsolutions.com>
2023-03-13 17:55:30 +0530

    global nav component

---
+++ Car-Showroom-p-3-CRUD/src/components/Nav.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav class="gallery_heading">
+        <h1>Car Gallery</h1>
+    </nav>
+</template>
+<style>
+.gallery_heading {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 30px;
+    font-size: 30px;
+}
+</style>
