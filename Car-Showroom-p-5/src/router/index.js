e4ea6f5a7bf6123582969b43f318a939699a8847
uday <uday.p@simformsolutions.com>
2023-03-22 11:29:19 +0530

    Js file to setup page navigation using Vue Router

---
+++ Car-Showroom-p-5/src/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/AppLogin.vue'),
+  },
+  {
+    path: '/registration',
+    name: 'registration',
+    component: () => import('../views/AppReg.vue')
+  },
+  {
+    path: '/detail/:id',
+    name: 'detail',
+    component: () => import('../views/carDetail.vue')
+  }
+]
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  linkExactActiveClass: 'active_nav_link'
+})
+
+// console.log(data.data().is_logged_in);
+
+export default router
