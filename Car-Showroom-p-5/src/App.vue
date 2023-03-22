58e8c6a61c0a780c60f9a257284fadbb9b28e48c
uday <uday.p@simformsolutions.com>
2023-03-22 11:33:43 +0530

    Main app component for routing views and another component rendering

---
+++ Car-Showroom-p-5/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Nav :is_logged_in="is_logged_in"></Nav>
+  <router-view></router-view>
+  
+</template>
+<script>
+import Home from "./views/HomeView.vue";
+
+export default {
+  components:{Home},
+  data() {
+    return {
+      is_logged_in:false,
+    }
+  },
+};
+</script>
