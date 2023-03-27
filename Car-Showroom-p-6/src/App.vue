23e35d4317b9d9f1ead7be7bfd708e0bb36734fa
uday <uday.p@simformsolutions.com>
2023-03-27 17:36:08 +0530

    main App component

---
+++ Car-Showroom-p-6/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <Nav :is_logged_in="is_logged_in"></Nav>
+  <router-view v-slot="{ Component }">
+    <transition name="route" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
+  
+</template>
+<script>
+import Home from "./views/HomeView.vue";
+
+export default {
+  components:{Home},
+  data() {
+    return {
+      is_logged_in:false,
+    }
+  },
+};
+</script>
+<style>
+.route-enter-from{
+  opacity: 0;
+  transform: translateX(100px);
+}
+.route-enter-active{
+  transition: all 0.3s ease-out;
+}
+.route-leave-to{
+  opacity: 0;
+  transform: translateX(-100px);
+}
+.route-leave-active{
+  transition: all 0.3s ease-in;
+}
+</style>
