0ade4badf88723f36284a90e983bb492a83ae49c
uday <uday.p@simformsolutions.com>
2023-03-27 17:33:28 +0530

    router configurations for SPA routing

---
+++ Car-Showroom-p-6/src/router/index.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/AppLogin.vue'),
+  },
+  {
+    path: '/registration',
+    name: 'registration',
+    component: () => import('../views/AppReg.vue')
+  },
+  {
+    path: '/detail/:id',
+    name: 'detail',
+    component: () => import('../views/CarDetail.vue')
+  }
+]
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  linkExactActiveClass: 'active_nav_link'
+})
+
+export default router
