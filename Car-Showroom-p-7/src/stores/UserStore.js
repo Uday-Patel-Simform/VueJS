3edb57798c8f45d0201324050d8bb1d27b984e86
uday <uday.p@simformsolutions.com>
2023-04-03 10:07:09 +0530

    global pinia user store

---
+++ Car-Showroom-p-7/src/stores/UserStore.js
@@ -0,0 +1,72 @@
+import axios from 'axios'
+import { defineStore } from 'pinia'
+import router from '../router';
+
+export const useUserStore = defineStore('UserStore', {
+    state: () => ({
+        isLoggedIn: false,
+        isValid: false,
+        vAlert: null
+    }),
+    actions: {
+        displayAlert(el, msg) {
+            this.isValid = true
+            el.style.border = "1px solid red"
+            this.vAlert = el.id + msg
+            return false;
+        },
+        removeAlert(el) {
+            el.style.border = "1px solid #c5c5c5"
+            this.isValid = false
+            this.vAlert = null
+            return
+        },
+        isValidEmail(e) {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!emailRegex.test(e.target.value)) {
+                this.displayAlert(e.target, ' is not valid*')
+            } else this.removeAlert(e.target)
+        },
+        isValidPassword(e) {
+            const check_length = /.{8,12}/
+            const check_upperCase = /(?=.*[A-Z])/
+            const check_lowerCase = /(?=.*[a-z])/
+            const check_special_char = /(?=.*[!@#$%^&*])/
+            const check_numeric = /(?=.*\d)/
+
+            if (!check_length.test(e.target.value)) {
+                this.displayAlert(e.target, ' must be 8-12 characters long*')
+            } else if (!check_upperCase.test(e.target.value)) {
+                this.displayAlert(e.target, ' must contain at least one Uppercase letter*')
+            } else if (!check_lowerCase.test(e.target.value)) {
+                this.displayAlert(e.target, ' must contain at least one Lowercase letter*')
+            } else if (!check_upperCase.test(e.target.value)) {
+                this.displayAlert(e.target, ' must contain at least one Uppercase letter*')
+            } else if (!check_special_char.test(e.target.value)) {
+                this.displayAlert(e.target, ' must contain at least one special character*')
+            } else if (!check_numeric.test(e.target.value)) {
+                this.displayAlert(e.target, ' must contain at least one numeric digit*')
+            } else this.removeAlert(e.target)
+        },
+        registerUser(user) {
+            axios.post("https://testapi.io/api/dartya/resource/users", user)
+                .then((res) => {
+                    if (res.status === 201) {
+                        router.push({ name: 'login' })
+                    }
+                })
+                .catch((e) => alert(e.message));
+        },
+        loginUser(user) {
+            axios.post("http://localhost:3000/login", user)
+                .then((response) => {
+                    if (response.status == 200) {
+                        router.push({ name: 'home' })
+                        this.isLoggedIn = true
+                    }
+                }).catch((e) => {
+                    alert(e)
+                })
+        },
+    }
+})
