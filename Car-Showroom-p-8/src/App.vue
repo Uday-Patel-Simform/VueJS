0eb012345fa84018a558d01de986ccdd9c8deb3a
uday <uday.p@simformsolutions.com>
2023-04-06 14:43:01 +0530

    App component for component based rendering

---
+++ Car-Showroom-p-8/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <Nav></Nav>
+  <router-view v-slot="{ Component }">
+    <transition name="route" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
+  
+</template>
+<script>
+import Home from "./views/HomeView.vue";
+
+export default {
+  components:{Home},
+};
+</script>
+<style>
+.route-enter-from{
+  opacity: 0;
+  transform: translateX(100px);
+}
+.route-enter-active{
+  transition: all 0.3s ease;
+}
+.route-leave-to{
+  opacity: 0;
+  transform: translateX(-100px);
+}
+.route-leave-active{
+  transition: all 0.3s ease-in;
+}
+</style>
