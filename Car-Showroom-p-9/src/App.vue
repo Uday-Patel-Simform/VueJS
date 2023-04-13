56c85d104b14fdf14b66c9207f1d8d5bf22ebe52
uday <uday.p@simformsolutions.com>
2023-04-13 17:20:00 +0530

    added log statement for production, staging and development mode

---
+++ Car-Showroom-p-9/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <Nav></Nav>
+  <router-view v-slot="{ Component }">
+    <transition name="route" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
+  
+</template>
+<script>
+import Home from "./views/HomeView.vue"
+
+export default {
+  components:{Home},
+  created(){
+    console.log(import.meta.env.MODE,'mode')
+  }
+}
+</script>
+<style>
+.route-enter-from{
+  opacity: 0;
+  transform: translateX(100px);
+}
+.route-enter-active{
+  transition: all 0.3s ease;
+}
+.route-leave-to{
+  opacity: 0;
+  transform: translateX(-100px);
+}
+.route-leave-active{
+  transition: all 0.3s ease-in;
+}
+</style>
