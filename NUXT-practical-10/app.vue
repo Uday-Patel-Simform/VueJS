c62abfcce49f69f4835edef01f3aa2ed79741d9d
uday <uday.p@simformsolutions.com>
2023-05-22 13:30:32 +0530

    index display page/ home page to loade components and pages

---
+++ NUXT-practical-10/app.vue
@@ -0,0 +1,23 @@
+<template>
+  <Nav></Nav>
+  <NuxtPage></NuxtPage>
+</template>
+<style>
+.route-enter-from {
+  opacity: 0;
+  transform: translateX(100px);
+}
+
+.route-enter-active {
+  transition: all 0.3s ease;
+}
+
+.route-leave-to {
+  opacity: 0;
+  transform: translateX(-100px);
+}
+
+.route-leave-active {
+  transition: all 0.3s ease-in;
+}
+</style>
