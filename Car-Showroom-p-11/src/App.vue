18f117fe30cbf78409ae3d806f6f320129b43511
uday <uday.p@simformsolutions.com>
2023-05-30 17:10:08 +0530

    main app.vue file to display global components and router-view components

---
+++ Car-Showroom-p-11/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <Nav></Nav>
+  <Suspense>
+    <router-view v-slot="{ Component }">
+      <transition name="route" mode="out-in">
+          <component :is="Component"></component>
+      </transition>
+    </router-view>
+  </Suspense>
+</template>
+<script setup>
+console.log(import.meta.env.MODE, 'mode')
+</script>
+<style>
+.route-enter-from {
+  opacity: 0;
+  transform: translateX(100px);
+}
+
+.route-enter-active {
+  transition: all 0.3s ease;
+}
+
+.route-leave-to {
+  opacity: 0;
+  transform: translateX(-100px);
+}
+
+.route-leave-active {
+  transition: all 0.3s ease-in;
+}
+</style>
